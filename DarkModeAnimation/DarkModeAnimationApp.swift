3f22ef5e00212b2c3139c4aad6708bf0601cc132
christoseteoglou <itsmeglue@gmail.com>
2023-11-29 14:46:15 +0100

    Initial Commit

---
+++ DarkModeAnimation/DarkModeAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModeAnimationApp.swift
+//  DarkModeAnimation
+//
+//  Created by Christos Eteoglou on 2023-11-29.
+//
+
+import SwiftUI
+
+@main
+struct DarkModeAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
