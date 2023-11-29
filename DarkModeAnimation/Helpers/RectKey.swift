3e972fed0461d179bca3947c53a2f1c149021675
christoseteoglou <itsmeglue@gmail.com>
2023-11-29 16:44:56 +0100

    Dark Mode Animation

---
+++ DarkModeAnimation/Helpers/RectKey.swift
@@ -0,0 +1,8 @@
+//
+//  RectKey.swift
+//  DarkModeAnimation
+//
+//  Created by Christos Eteoglou on 2023-11-29.
+//
+
+import Foundation
---
+++ DarkModeAnimation/Helpers/View+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  View+Extension.swift
+//  DarkModeAnimation
+//
+//  Created by Christos Eteoglou on 2023-11-29.
+//
+
+import SwiftUI
+
+struct View_Extension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View_Extension()
+}
